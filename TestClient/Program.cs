12aae60b6fe38c97539cc10d41fdd597d1d39644
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2015-04-07 13:40:11 -0300

    Test Projects added

---
+++ TestClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using Hermes;
+
+namespace TestClient
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			var subject = new Subject<int> ();
+
+			subject.OnError (new Exception ());
+			subject.OnCompleted ();
+
+			Console.WriteLine ("Starting Test MQTT Client...");
+
+			var configuration = new ProtocolConfiguration {
+				BufferSize = 128 * 1024,
+				Port = Protocol.DefaultNonSecurePort,
+				KeepAliveSecs = 0,
+				WaitingTimeoutSecs = 5
+			};
+			var initializer = new ClientInitializer (hostAddress: "127.0.0.1");
+			var client = initializer.Initialize (configuration);
+			var connected = ConnectAsync (client, new ClientCredentials ("testClient")).Result;
+
+			if(connected)
+					Console.WriteLine ("MQTT Client connected successfully...");
+			else
+					Console.WriteLine ("MQTT has not been connected...");
+
+			Console.ReadKey ();
+		}
+
+		private static async Task<bool> ConnectAsync (IClient client, ClientCredentials credentials, bool cleanSession = false)
+		{
+			try {
+				await client.ConnectAsync (credentials, cleanSession);
+			} catch (ClientException clientEx) {
+				var message = clientEx.Message;
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ TestServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using Hermes;
+
+namespace TestServer
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			Console.WriteLine ("Starting Test MQTT Server...");
+
+			var configuration = new ProtocolConfiguration {
+				BufferSize = 128 * 1024,
+				Port = Protocol.DefaultNonSecurePort,
+				KeepAliveSecs = 0,
+				WaitingTimeoutSecs = 5,
+			};
+			var initializer = new ServerInitializer ();
+			var server = initializer.Initialize (configuration);
+
+			server.Start ();
+
+			Console.WriteLine ("MQTT Server Started Successfully...");
+
+			while (true) {
+				Thread.Sleep (1000);
+
+				var clients = server.ActiveClients;
+			}
+		}
+	}
+}
